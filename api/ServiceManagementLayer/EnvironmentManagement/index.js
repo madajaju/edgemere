6b4ed8e1d8a1c045a8421610523c54042d411978
madajaju <darren@pulsipher.org>
2020-06-19 11:13:29 -0700

    EDGE-9 Software Defined Infrastructure Documentation

--- api/ServiceManagementLayer/EnvironmentManagement/index.js
+++ api/ServiceManagementLayer/EnvironmentManagement/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     shortname: 'em',
     name: 'Environment Management',
-    description: 'Environment Management is a package that contains...',
-    color: '#lightblue'
+    description: 'Environment Management is a package that contains models and services for managing different environments',
+    color: '#lightgreen',
+    depends: [ "Service Orchestrator" ]
 };
