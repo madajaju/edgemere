e361cc7be9c49cd99282c1b8198a122da84cb607
madajaju <darren@pulsipher.org>
2020-10-31 23:01:47 -0600

    edgemere-5 Integrate microservice deployment and build fr om ailtire 0.1.1

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -2,22 +2,28 @@
 module.exports = {
     name: 'edgemere',
     contexts: {
-        dev: {
+        local: {
             type: 'swarm',
             tag: 'edgemere_dev',
             file: 'docker-compose.yml',
+            env: {}
+        },
+        dev: {
+            type: 'swarm',
+            tag: 'edgemere_dev',
+            file: 'docker-compose-full.yml',
             env: {}
         },
         test: {
             type: 'swarm',
             tag: 'edgemere_dev',
-            file: 'docker-compose.yml',
+            file: 'docker-compose-full.yml',
             env: {}
         },
         prod: {
             type: 'swarm',
             tag: 'edgemere_dev',
-            file: 'docker-compose.yml',
+            file: 'docker-compose-full.yml',
             env: {}
         }
     }
